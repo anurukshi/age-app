074f8688b81de25d5a80537f32ff62a2f4474067
anurukshi <amk1001862@student.hamk.fi>
2024-10-18 21:58:59 +0300

    Initial commit

---
+++ age app/src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int age = 10;
+        if (age > 0 && age < 18){
+            System.out.println("You are underage.");
+        }
+        
+        {
+            System.out.println("You are an adult.");
+        }
+            System.out.println("Press space to exit.");
+        
+
+
+
+
+
+
+    }
+
+
+}
