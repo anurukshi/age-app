5e0cf17441e692767bb331eb5c982a4144933da8
anurukshi <amk1001862@student.hamk.fi>
2024-10-18 22:30:21 +0300

    added condition under 15 age can drive a moped and tested

--- age app/src/App.java
+++ age app/src/App.java
@@ -1,19 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int age = 66;
-        if (age > 0 && age < 18){
+        int age = 60;
+        if (age > 0 && age < 18)
+        {
             System.out.println("You are underage.");
-        }
-        else if (age >= 65)
+            if (age >= 15)
+        {
+        System.out.println("You can drive a moped.");
+    }
+    }
+            else if (age >= 65)
         {
             System.out.println("You are retired.");
-        }
+    }
         else
-        {
-            System.out.println("You are an adult.");
-        }
-            
+            {
+                System.out.println("You are an adult.");
+            }
         
 
 
