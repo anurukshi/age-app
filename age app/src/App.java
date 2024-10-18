968c592cccbc359f0c61413dbab90284afb297e5
anurukshi <amk1001862@student.hamk.fi>
2024-10-18 22:03:30 +0300

    age changed from 10 to 20

--- age app/src/App.java
+++ age app/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int age = 10;
+        int age = 20;
         if (age > 0 && age < 18){
             System.out.println("You are underage.");
         }
