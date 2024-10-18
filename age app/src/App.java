bdbba17eb133f361b144b3795f7ba819d3320893
anurukshi <amk1001862@student.hamk.fi>
2024-10-18 22:11:53 +0300

    added two more conditions and tested with different values

--- age app/src/App.java
+++ age app/src/App.java
@@ -1,15 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        int age = 20;
+        int age = 66;
         if (age > 0 && age < 18){
             System.out.println("You are underage.");
         }
-        
+        else if (age >= 65)
+        {
+            System.out.println("You are retired.");
+        }
+        else
         {
             System.out.println("You are an adult.");
         }
-            System.out.println("Press space to exit.");
+            
         
 
 
